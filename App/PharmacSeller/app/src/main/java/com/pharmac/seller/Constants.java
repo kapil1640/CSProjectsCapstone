ecd58e55acc24478eb37fd282f1d830c3f9316fe
kapil1640 <kuchekulla.kapil@gmail.com>
2022-07-20 15:05:24 -0500

    PharmaC_Capstone_Project

---
+++ App/PharmacSeller/app/src/main/java/com/pharmac/seller/Constants.java
@@ -0,0 +1,39 @@
+package com.pharmac.seller;
+
+public class Constants {
+
+    public static final String FCM_KEY = "AAAA9xV4t7Y:APA91bFh-pZ0yrLPu1BXU8NBwECFdSSvRTA51xgiB75FdZ1hh2Cmrxt_l-ND2oLmrefa6BFSV8myIhtIxqIdI9XOJYsk-Kg--u9Ty1e4rgANfWzEna-SdVaj9sIKSNyoaqJ5SNjFNMA8";
+    public static final String FCM_TOPIC = "PUSH_NOTIFICATIONS";
+
+    //product categories
+    public static final String[] productCategories = {
+            "Capsules",
+            "Suppositories",
+            "Drops",
+            "Inhalers",
+            "Injections",
+            "Implants ",
+            "sugar",
+            "diabetis",
+            "cancer",
+            "corona",
+            "weighted",
+            "non-weighted"
+    };
+
+    public static final String[] productCategories1 = {
+            "Capsules",
+            "Suppositories",
+            "Drops",
+            "Inhalers",
+            "Injections",
+            "Implants ",
+            "sugar",
+            "diabetis",
+            "cancer",
+            "corona",
+            "weighted",
+            "non-weighted"
+    };
+
+}
